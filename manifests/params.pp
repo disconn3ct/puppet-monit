e71117b63e3cd6bf1fa5d585949aabe20ed979ac
Steven Kurylo <skurylo@xmatters.com>
2015-01-21 15:44:12 -0800

    Make existing tests pass

---
+++ manifests/params.pp
@@ -0,0 +1,25 @@
+class monit::params {
+
+  case $::osfamily {
+    'Debian': {
+      $config = '/etc/monit/monitrc'
+      $included = '/etc/monit/monit.d'
+      $idfile = '/var/lib/monit/id'
+      $statefile = '/var/lib/monit/state'
+      $basedir = '/var/lib/monit/events'
+    }
+    'RedHat': {
+      $config = '/etc/monit.conf' #/etc/monit/monitrc
+      $included = '/etc/monit.d' #/etc/monit/monit.d
+      $idfile = '/var/.monit.id' #'/var/lib/monit/id'
+      $statefile = '/var/.monit.state' #/var/lib/monit/state
+      $basedir = '/var/monit' #/var/lib/monit/events
+    }
+    default: {
+      fail("Class monit does not support ${::operatingsystem}")
+    }
+
+  }
+
+
+}
